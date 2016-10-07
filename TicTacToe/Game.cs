2171191cc6eb28a73328870e490d7f17b6921a5f
Carnitter <rottor467@gmail.com>
2016-10-07 11:23:45 +0200

    hey
    hey

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        List<Play> plays = new List<Play>();
+
+        public Game()
+        {
+
+        }
+
+        public void update(Play play)
+        {
+            plays.Add(play);
+            hasWon();
+
+        }
+
+        public int hasWon()
+        {
+            
+
+        }
+
+
+        
+    }
+}
---
+++ TicTacToe/Play.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Play
+    {
+        int x;
+        int y;
+        int playerNumber;
+
+        public Play(int x, int y, int playerNumber)
+        {
+            this.x = x;
+            this.y = y;
+            this.playerNumber = playerNumber;
+
+        }
+
+
+    }
+}
